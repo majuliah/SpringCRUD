0ad95336b77ea3d79f302f8ad8cbe4441a3e2757
Majulia <majulialcarvalho@gmail.com>
2024-05-16 22:17:16 -0300

    update e delete conlcuido

--- src/main/java/br/com/crud/livro/controller/LivroController.java
+++ src/main/java/br/com/crud/livro/controller/LivroController.java
@@ -3,18 +3,21 @@ package br.com.crud.livro.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 
 import br.crud.livro.entity.Categoria;
 import br.crud.livro.entity.Livro;
 import br.crud.livro.entity.Modelo;
 import br.crud.livro.repository.LivroRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 //classe controller -> spring busca para fazer requisições
 @Controller
@@ -36,6 +39,22 @@ public class LivroController {
     public void salvar(@RequestBody Livro livro) {
 
         livroRepository.save(livro);
+    }
+
+    @ResponseBody
+    @Transactional
+    @RequestMapping(path = "/livros", method = RequestMethod.PUT)
+    public void atualizar(@RequestBody Livro livro) {
+        livroRepository.save(livro);
+    }
+
+    @ResponseBody
+    @Transactional
+    @RequestMapping(path = "/livros/{isbn}", method = RequestMethod.DELETE)
+    public void deletar(@PathVariable Long isbn){
+
+        livroRepository.deleteById(isbn);
+
     }
 
 }
