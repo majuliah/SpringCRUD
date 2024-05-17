ac01fca445da43b06e81239c910150084805516c
Majulia <majulialcarvalho@gmail.com>
2024-05-16 22:21:30 -0300

    refatorando controller

--- src/main/java/br/com/crud/livro/controller/LivroController.java
+++ src/main/java/br/com/crud/livro/controller/LivroController.java
@@ -1,39 +1,27 @@
 package br.com.crud.livro.controller;
 
-import java.util.Arrays;
+import br.crud.livro.entity.Livro;
+import br.crud.livro.repository.LivroRepository;
+
 import java.util.List;
-
-import java.util.Arrays;
-import java.util.List;
-
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-
-import br.crud.livro.entity.Categoria;
-import br.crud.livro.entity.Livro;
-import br.crud.livro.entity.Modelo;
-import br.crud.livro.repository.LivroRepository;
-
+import javax.transaction.Transactional;
 
 //classe controller -> spring busca para fazer requisições
-@Controller
+@RestController
 public class LivroController {
 
     @Autowired
     private LivroRepository livroRepository;
 
-    @ResponseBody
     @RequestMapping("/livros")
     public List<Livro> listar() {
         List<Livro> livros = livroRepository.findAll();
         return livros;
     }
 
-    @ResponseBody
     @Transactional
     @RequestMapping(path = "/livros", method = RequestMethod.POST)
     public void salvar(@RequestBody Livro livro) {
@@ -41,14 +29,12 @@ public class LivroController {
         livroRepository.save(livro);
     }
 
-    @ResponseBody
     @Transactional
     @RequestMapping(path = "/livros", method = RequestMethod.PUT)
     public void atualizar(@RequestBody Livro livro) {
         livroRepository.save(livro);
     }
 
-    @ResponseBody
     @Transactional
     @RequestMapping(path = "/livros/{isbn}", method = RequestMethod.DELETE)
     public void deletar(@PathVariable Long isbn){
